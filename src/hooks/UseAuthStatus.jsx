9d4e4332548a64b9d81ed7c6d107fcdc624981f7
saleemdummar <saleemdummar@gmail.com>
2024-02-10 21:59:02 +0500

    add the functionality to the profile for update name in fireStore and authenticaton

--- src/hooks/UseAuthStatus.jsx
+++ src/hooks/UseAuthStatus.jsx
@@ -6,16 +6,12 @@ export function UseAuthStatus() {
   const [checkingStatus, setCheckingStatus] = useState(true);
   useEffect(() => {
     const auth = getAuth();
-    onAuthStateChanged(
-      auth,
-      (user) => {
-        if (user) {
-          setLoggedIn(true);
-        }
-        setCheckingStatus(false);
-      },
-      []
-    );
-  });
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setLoggedIn(true);
+      }
+      setCheckingStatus(false);
+    });
+  }, []);
   return { loggedIn, checkingStatus };
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,14 @@
-import { getAuth } from "firebase/auth";
+import { getAuth, updateProfile } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import React, { useState } from "react";
+import { toast } from "react-toastify";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase";
 
 export default function Profile() {
   const auth = getAuth();
   const navigate = useNavigate();
+  const [changeDetail, setChangeDetatil] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -13,6 +17,31 @@ export default function Profile() {
   function onLogout() {
     auth.signOut();
     navigate("/");
+  }
+  function onChange(e) {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value,
+    }));
+  }
+  async function onSubmit() {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        // update the display name in firebase auth
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+
+        // update the name in the fireStore
+        const docRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(docRef, {
+          name,
+        });
+      }
+      toast.success("Profile detail updated successfully");
+    } catch (error) {
+      toast.error("Something went wrong Could not update the profile detail");
+    }
   }
   return (
     <>
@@ -25,22 +54,31 @@ export default function Profile() {
               type='text'
               id='name'
               value={name}
-              disabled
-              className='w-full mb-6 px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out'
+              onChange={onChange}
+              disabled={!changeDetail}
+              className={`w-full mb-6 px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${
+                changeDetail && "bg-red-200 focus:bg-red-200"
+              }`}
             />
             {/* input for email */}
             <input
               type='email'
               id='email'
               value={email}
+              // onChange={onChange}
               disabled
               className='w-full mb-6 px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out'
             />
             <div className='mb-6 flex justify-between whitespace-nowrap sm:text-lg'>
               <p className='flex items-center'>
                 Do you want to change your name?
-                <span className='text-red-600 hover:text-red-700 cursor-pointer transition ease-in-out duration-200 ml-1'>
-                  Edit
+                <span
+                  onClick={() => {
+                    changeDetail && onSubmit();
+                    setChangeDetatil((prevState) => !prevState);
+                  }}
+                  className='text-red-600 hover:text-red-700 cursor-pointer transition ease-in-out duration-200 ml-1'>
+                  {changeDetail ? "Apply change" : "Edit"}
                 </span>
               </p>
               <p
