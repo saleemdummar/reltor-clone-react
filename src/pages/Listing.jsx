ea53b370cc696b36ca79cc12a5be5f7831678b31
saleemdummar <saleemdummar@gmail.com>
2024-02-18 18:19:55 +0500

    create listing page and install swiper js and make image slider effect

---
+++ src/pages/Listing.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { db } from "../firebase";
+import { doc, getDoc } from "firebase/firestore";
+import Spinner from "../components/Spinner";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectFade, Autoplay, Navigation, Pagination } from "swiper/modules";
+import SwiperCore from "swiper";
+import "swiper/css/bundle";
+
+export default function Listing() {
+  const params = useParams();
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+  SwiperCore.use([Autoplay, Navigation, Pagination]);
+  useEffect(() => {
+    async function fetchListing() {
+      const docRef = doc(db, "listings", params.listingId);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        setListing(docSnap.data());
+        setLoading(false);
+        console.log(listing);
+      }
+    }
+    fetchListing();
+  }, [params.listingId]);
+  if (loading) {
+    return <Spinner />;
+  }
+  return (
+    <main>
+      <Swiper
+        slidesPerView={1}
+        navigation
+        pagination={{ type: "progressbar" }}
+        effect='fade'
+        modules={[EffectFade]}
+        autoplay={{ delay: 3000 }}>
+        {listing.imgUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <img
+              className='relative w-full overflow-hidden h-[300px]'
+              style={{ center: "no-repeat", backgroundSize: "cover" }}
+              src={listing.imgUrls[index]}
+              alt=''
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </main>
+  );
+}
